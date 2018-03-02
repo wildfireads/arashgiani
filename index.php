0d5735f92ba8aa4c22988446301ef665fbc5c059
Arash <arashgiani84@gmail.com>
2018-03-02 15:34:47 -0500

    t1

--- index.php
+++ index.php
@@ -10,12 +10,26 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
     <body>
-        <!--[if lte IE 9]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-        <![endif]-->
 
- 
-        
+        <div class="container" style="background-color: #fafafa; margin-bottom: 50px; min-height: 350px">
+            <h2>Abstrakt</h2>
+            <div class="row">
+                <div class="col-7" style="padding-right: 25px;">
+                    <p>Nulla facilisi. Donec suscipit condimentum lectus malesuada sollicitudin. Pellentesque varius, nisi non posuere congue, erat odio mattis libero, ut efficitur arcu velit posuere erat. Phasellus eget lorem a risus bibendum finibus cursus facilisis neque. Nam orci turpis, dictum eu blandit vel, ultricies in ligula. Suspendisse sodales mi eget purus finibus dignissim. Aliquam ultrices lorem metus, at consequat justo hendrerit feugiat.</p>
+                    <p>Nunc commodo placerat leo, vitae semper arcu accumsan tincidunt. Duis ullamcorper quam non neque posuere, nec sollicitudin neque eleifend. Cras efficitur in massa in ultricies. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                    <button type="button" class="btn btn-primary btn-lg">Large button</button>
+                    <button type="button" class="btn btn-secondary btn-lg">Large button</button>
+                    </br>
+                    </br>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
 
 
 
