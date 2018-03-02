416112c7530f6cc4c20bfdcf24c2e9d9f5860dca
Arash <arashgiani84@gmail.com>
2018-03-01 19:42:24 -0500

    layout

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
  
 
  
+<div>
 
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
 
   <a class="navbar-brand" href="#">Arash Giani</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -38,7 +38,7 @@
   </div>
 </nav>
 
-
+<div>
 
 
 
