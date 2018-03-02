7a9fe80282587efcb5273dbc8f71c5efa171bd1a
Arash <arashgiani84@gmail.com>
2018-03-01 19:50:02 -0500

    menu

--- index.php
+++ index.php
@@ -15,30 +15,43 @@
  
 
  
-<div>
+<section class="menu cid-qB8KHSwX7D" once="menu" id="menu2-d" data-rv-view="5931">
 
-<nav class="navbar navbar-expand-lg navbar-inverse bg-inverse justify-content-between">
-
-  <a class="navbar-brand" href="#">Arash Giani</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active"><a class="nav-link" href="#">Home</a></li>
-      <li class="nav-item"><a class="nav-link" href="#">Photography</a></li>
-      <li class="nav-item"><a class="nav-link" href="#">Design</a></li>
-      <li class="nav-item"><a class="nav-link" href="#">Development</a></li>
-      <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
-    </ul>
     
 
-  </div>
-</nav>
-
-<div>
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm bg-color transparent">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="https://mobirise.com/bootstrap-4-theme/">
+                        MAGAZINE TEMPLATE</a></span>
+            </div>
+        </div>
+         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item dropdown"><a class="nav-link link dropdown-toggle text-white display-4" href="#" data-toggle="dropdown-submenu" aria-expanded="false">
+                        
+                        BLOCK DEMOS 1</a><div class="dropdown-menu"><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/slider.html">Slider Header Demo</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/video-background.html">Video Background Demo</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/footer-template.html">Footer Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/table-template.html">Table Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/carousel-template.html">Carousel Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/blog.html">Gallery Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/menu-template.html">Menu Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/navigation-menu-template.html">Navigation Menu Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/navbar-template.html">Navbar Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/header-template.html">Header Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/registration-form-template.html">Registration Form Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/grid-template.html">Grid Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/social-network-template.html">Social Network Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/parallax-template.html">Parallax Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/parallax-scrolling-template.html">Parallax Scrolling Template</a></div></li>
+                <li class="nav-item dropdown open"><a class="nav-link link dropdown-toggle text-white display-4" href="#" data-toggle="dropdown-submenu" aria-expanded="true">
+                        
+                        BLOCK DEMOS 2</a><div class="dropdown-menu"><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/shopping-cart-template.html">Shopping Cart Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/contact-form-template.html">Contact Form Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/list-template.html">List Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/video-template.html">Video Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/photo-gallery-template.html">Photo Gallery Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/map-template.html">Map Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/faq-template.html">FAQ Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/slider-template.html">Slider Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/background-template.html">Video Background Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/documentation-template.html">Documentation Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/article-template.html">Article Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/google-maps-template.html">Google Maps Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/form-templates.html">Form Templates </a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/tab-template.html">Tab Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/video-gallery-template.html">Video Gallery Template</a></div></li>
+                
+                <li class="nav-item dropdown">
+                    <a class="nav-link link dropdown-toggle text-white display-4" href="#" data-toggle="dropdown-submenu" aria-expanded="false">SITE TEMPLATES</a><div class="dropdown-menu"><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/one-page.html">One Page Template<br></a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/blog.html">Blog Template Demo<br></a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/real-estate-template/">Real Estate Template<br></a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/restaurant-template/">Restaurant Template<br></a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/travel-template/">Travel Template<br></a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/wedding-template/">Wedding Template<br></a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/hotel-template/">Hotel Template<br></a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/landing-page-template/">Landing Page Template<br></a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/news-template/">News Template<br></a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/profile-template/">Profile Template<br></a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/education-template/">Education Template<br></a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/coming-soon-template/">Coming Soon Template<br></a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/error-page-template/">Error Page Template<br></a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/event-template/">Event Template<br></a></div>
+                </li><li class="nav-item dropdown"><a class="nav-link link dropdown-toggle text-white display-4" href="#" aria-expanded="false" data-toggle="dropdown-submenu">SITE TEMPLATES 2</a><div class="dropdown-menu"><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/about-us-page-template/" aria-expanded="false">About Us Page Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/agency-template/" aria-expanded="false">Agency Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/application-template/" aria-expanded="false">Application Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/business-template/" aria-expanded="false">Business Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/corporate-template/" aria-expanded="false">Corporate Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/homepage-template/" aria-expanded="false">Homepage Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/magazine-template/" aria-expanded="false">Magazine Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/agency-template/multi-page-template.html" aria-expanded="false">Multi Page Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/personal-website-template/" aria-expanded="false">Personal Website Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/photography-template/" aria-expanded="false">Photography Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/store-template/" aria-expanded="false">Store Template</a><a class="dropdown-item text-white display-4" href="https://mobirise.com/bootstrap-4-theme/web-application-template/" aria-expanded="false">Web Application Template</a></div></li></ul>
+            <div class="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-primary display-4" href="https://mobirise.com/bootstrap-4-theme/index.html#header5-cy">
+                    
+                    FREE DOWNLOAD</a></div>
+        </div>
+    </nav>
+</section>
 
 
 
