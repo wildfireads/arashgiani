57d187b8cef96ed4c666975bf2006d36a2e3b65d
Arash <arashgiani84@gmail.com>
2018-03-02 16:04:00 -0500

    Pinterest

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
                 <div class="col-2">facebook</div>
                 <div class="col-2">Twitter</div>
                 <div class="col-2">Instagram</div>
-                <div class="col-2">Medium</div>
+                <div class="col-2">Pinterest</div>
                 <div class="col-2">Behance</div>
                 <div class="col-2">GITHub</div>
             </div>
