da3c19deda683ab2c61d328aabb2ad9dc67b17f2
Arash <arashgiani84@gmail.com>
2018-03-02 15:56:37 -0500

    art

--- index.php
+++ index.php
@@ -37,8 +37,7 @@
         </div>
 
         <div class="container" style="background-color: #fafafa; text-align: center; margin-bottom: 50px;">
-
-            <h2>Fotografie</h2>
+            <h2>Photography</h2>
             <p>Curabitur non imperdiet risus, sit amet facilisis arcu. Suspendisse potenti. Pellentesque luctus enim ut ex mattis consequat. Sed lobortis libero nisl, non venenatis ex imperdiet ut. Vivamus eget aliquet nunc. Nullam et eros in ipsum egestas semper rhoncus accumsan magna.</p>
             </br>
                 <div class="row" style="margin-bottom: 25px">
@@ -52,6 +51,18 @@
                 <div class="col-4" style="height: 200px; background-color: #bbb">Thumbnail</div>
                 <div class="col-4" style="height: 200px; background-color: #ccc">Thumbnail</div>
             </div>
+        </div>
+
+
+        <div class="container" style="background-color: #fafafa; text-align: center; margin-bottom: 50px;">
+            <h2>Art</h2>
+            <p>Curabitur non imperdiet risus, sit amet facilisis arcu. Suspendisse potenti. Pellentesque luctus enim ut ex mattis consequat. Sed lobortis libero nisl, non venenatis ex imperdiet ut. Vivamus eget aliquet nunc. Nullam et eros in ipsum egestas semper rhoncus accumsan magna.</p>
+            </br>
+                <div class="row" style="margin-bottom: 25px">
+                <div class="col-4" style="height: 200px; background-color: #aaa">Thumbnail</div>
+                <div class="col-4" style="height: 200px; background-color: #bbb">Thumbnail</div>
+                <div class="col-4" style="height: 200px; background-color: #ccc">Thumbnail</div>
+            </div>
 
             <div class="row" style="margin-bottom: 25px">
                 <div class="col-4" style="height: 200px; background-color: #aaa">Thumbnail</div>
@@ -59,9 +70,6 @@
                 <div class="col-4" style="height: 200px; background-color: #ccc">Thumbnail</div>
             </div>
         </div>
-
-
-
 
 
 
