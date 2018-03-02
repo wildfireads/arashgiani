289003da45167639c4192330ed8532a45b30e42a
Arash <arashgiani84@gmail.com>
2018-03-01 20:24:08 -0500

    133

--- index.php
+++ index.php
@@ -36,10 +36,10 @@
 </div>
 
 <div class="row">
-    <div class="col" style="background-color: yellow; height: 200px;"> 3 of 3 </div>
     <div class="col" style="background-color: red; height: 200px;"> 2 of 3 </div>
     <div class="col" style="background-color: yellow; height: 200px;"> 1 of 3 </div>
     <div class="col" style="background-color: red; height: 200px;"> 2 of 3 </div>
+    <div class="col" style="background-color: yellow; height: 200px;"> 3 of 3 </div>
 </div>
 
 <div class="row">
