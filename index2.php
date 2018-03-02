1d17749aec1be8aa4e2b1c8d2979666f7f74e35f
Arash <arashgiani84@gmail.com>
2018-03-02 15:31:22 -0500

    switched

---
+++ index2.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Arash Giani</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+  </head>
+  <body>
+  
+
+<div class="container" style="background-color: #fafafa; margin-bottom: 50px; margin-top: 50px;">
+  Arash Giani
+</div>
+
+
+<div class="container" style="background-color: #fafafa; margin-bottom: 50px; min-height: 350px">
+  
+  <h2>Abstrakt</h2>
+  
+ <div class="row">
+ 	<div class="col-7" style="padding-right: 25px;">
+<p>Nulla facilisi. Donec suscipit condimentum lectus malesuada sollicitudin. Pellentesque varius, nisi non posuere congue, erat odio mattis libero, ut efficitur arcu velit posuere erat. Phasellus eget lorem a risus bibendum finibus cursus facilisis neque. Nam orci turpis, dictum eu blandit vel, ultricies in ligula. Suspendisse sodales mi eget purus finibus dignissim. Aliquam ultrices lorem metus, at consequat justo hendrerit feugiat.</p>
+
+
+
+<p>Nunc commodo placerat leo, vitae semper arcu accumsan tincidunt. Duis ullamcorper quam non neque posuere, nec sollicitudin neque eleifend. Cras efficitur in massa in ultricies. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+<button type="button" class="btn btn-primary btn-lg">Large button</button>
+<button type="button" class="btn btn-secondary btn-lg">Large button</button>
+</br>
+</br>
+
+ 
+
+
+</div>
+</div>
+</div>
+
+
+
+<div class="container" style="background-color: #fafafa; text-align: center; margin-bottom: 50px;">
+
+
+ <div class="row">
+  <div class="col-2">facebook</div>
+  <div class="col-2">Twitter</div>
+  <div class="col-2">Instagram</div>
+  <div class="col-2">Medium</div>
+  <div class="col-2">Behance</div>
+  <div class="col-2">GITHub</div>
+</div>
+
+
+</div>
+
+
+
+ 
+<div class="container" style="background-color: #fafafa; margin-bottom: 50px; min-height: 350px">
+  Photography
+
+
+ <div class="row">
+  <div class="col-4">col-4</div>
+  <div class="col-4">col-4</div>
+  <div class="col-4">col-4</div>
+</div>
+
+
+</div>
+
+<div class="container" style="background-color: #fafafa; margin-bottom: 50px; min-height: 350px">
+  Design
+</div>
+
+<div class="container" style="background-color: #fafafa; margin-bottom: 50px; min-height: 350px">
+  Development
+</div>
+
+<div class="container" style="background-color: #fafafa; margin-bottom: 50px; min-height: 350px">
+  Contact
+</div>
+
+<div class="container" style="background-color: #ebebeb; padding-top: 30px; padding-bottom: 30px;">
+  Footer
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
